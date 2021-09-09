c9812e9a7df0219dba210c9e33ebe9ab99d52313
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-09-08 22:37:58 -0700

    Complete exercise

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+'use strict'
+
 console.log("Hello World!\n==========\n");
+
+let watching = new Promise((resolve, reject) => {
+  let userWatchingLiveStream = true;
+
+  if (userWatchingLiveStream) {
+    resolve('Thumbs up and Subscribe!');
+  } else {
+    reject('User left.');
+  }
+});
+
+watching.then((message) => {
+  console.log(message);
+}).catch((error) => {
+  console.log(error);
+});
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
